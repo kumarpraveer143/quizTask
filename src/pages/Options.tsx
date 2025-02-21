88ee556fb11403b6fee589dcbb775dac87f26a25
kumarpraveer143 <kumarpraveernit@gmail.com>
2025-02-21 13:14:10 +0530

    some fix in home page

--- src/pages/Options.tsx
+++ src/pages/Options.tsx
@@ -3,20 +3,16 @@ import { useNavigate } from "react-router-dom";
 const Options = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex h-screen bg-gray-900 text-white">
+    <div className="flex flex-col sm:flex-row h-screen bg-gray-900 text-white">
       <div
-        className="w-1/2 flex items-center justify-center bg-blue-600 cursor-pointer hover:bg-blue-700 transition"
-        onClick={() => {
-          navigate("/mcq");
-        }}
+        className="flex-1 flex items-center justify-center bg-blue-600 cursor-pointer hover:bg-blue-700 transition h-1/2 sm:h-full"
+        onClick={() => navigate("/mcq")}
       >
         <h2 className="text-3xl font-bold">Quiz (MCQ)</h2>
       </div>
       <div
-        className="w-1/2 flex items-center justify-center bg-green-600 cursor-pointer hover:bg-green-700 transition"
-        onClick={() => {
-          navigate("/integer");
-        }}
+        className="flex-1 flex items-center justify-center bg-green-600 cursor-pointer hover:bg-green-700 transition h-1/2 sm:h-full"
+        onClick={() => navigate("/integer")}
       >
         <h2 className="text-3xl font-bold">Integer Type Problem</h2>
       </div>
